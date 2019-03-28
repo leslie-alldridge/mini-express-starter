47543e38a2a5f7305ad6a3910316dc848ba9ce29
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-03-29 09:54:34 +1300

    sending mock tickets
    sending mock tickets

---
+++ server.js
@@ -0,0 +1,201 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const server = express();
+
+server.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+server.use(bodyParser.json());
+
+//API calls
+server.get("/tickets", (req, res) => {
+  res.json({
+    tickets: [
+      {
+        id: 35436,
+        url: "https://company.zendesk.com/api/v2/tickets/35436.json",
+        external_id: "ahg35h3jh",
+        created_at: "2009-07-20T22:55:29Z",
+        updated_at: "2011-05-05T10:38:52Z",
+        type: "incident",
+        subject:
+          "Check if leaver has access to StatusPage, New Relic, DataDog and Sumo - Im Contracting [C] is leaving Xero - 2019 01 19",
+        raw_subject: "{{dc.printer_on_fire}}",
+        description: "The fire is very colorful.",
+        priority: "high",
+        status: "open",
+        recipient: "support@company.com",
+        requester_id: 20978392,
+        submitter_id: 76872,
+        assignee_id: 235323,
+        organization_id: 509974,
+        group_id: 98738,
+        collaborator_ids: [35334, 234],
+        follower_ids: [35334, 234],
+        problem_id: 9873764,
+        has_incidents: false,
+        due_at: null,
+        tags: ["enterprise", "other_tag", "ps_sre_monitoring", "user_workday"],
+        via: {
+          channel: "web"
+        },
+        custom_fields: [
+          {
+            id: 27642,
+            value: "745"
+          },
+          {
+            id: 27648,
+            value: "yes"
+          }
+        ],
+        satisfaction_rating: {
+          id: 1234,
+          score: "good",
+          comment: "Great support!"
+        },
+        sharing_agreement_ids: [84432]
+      },
+      {
+        id: 35437,
+        url: "https://company.zendesk.com/api/v2/tickets/35436.json",
+        external_id: "ahg35h3jh",
+        created_at: "2009-07-20T22:55:29Z",
+        updated_at: "2011-05-05T10:38:52Z",
+        type: "incident",
+        subject:
+          "Check if leaver has access to StatusPage, New Relic, DataDog and Sumo - User Donaldson [C] is leaving Xero - 2019 04 19",
+        raw_subject: "{{dc.printer_on_fire}}",
+        description: "The fire is very colorful.",
+        priority: "high",
+        status: "open",
+        recipient: "support@company.com",
+        requester_id: 20978392,
+        submitter_id: 76872,
+        assignee_id: 235323,
+        organization_id: 509974,
+        group_id: 98738,
+        collaborator_ids: [35334, 234],
+        follower_ids: [35334, 234],
+        problem_id: 9873764,
+        has_incidents: false,
+        due_at: null,
+        tags: ["enterprise", "other_tag", "user_workday", "ps_sre_monitoring"],
+        via: {
+          channel: "web"
+        },
+        custom_fields: [
+          {
+            id: 27642,
+            value: "745"
+          },
+          {
+            id: 27648,
+            value: "yes"
+          }
+        ],
+        satisfaction_rating: {
+          id: 1234,
+          score: "good",
+          comment: "Great support!"
+        },
+        sharing_agreement_ids: [84432]
+      },
+      {
+        id: 35437,
+        url: "https://company.zendesk.com/api/v2/tickets/35436.json",
+        external_id: "ahg35h3jh",
+        created_at: "2009-07-20T22:55:29Z",
+        updated_at: "2011-05-05T10:38:52Z",
+        type: "incident",
+        subject:
+          "Check if leaver has access to StatusPage, New Relic, DataDog and Sumo - User Bruiser is leaving Xero - 2019 02 18",
+        raw_subject: "{{dc.printer_on_fire}}",
+        description: "The fire is very colorful.",
+        priority: "high",
+        status: "open",
+        recipient: "support@company.com",
+        requester_id: 20978392,
+        submitter_id: 76872,
+        assignee_id: 235323,
+        organization_id: 509974,
+        group_id: 98738,
+        collaborator_ids: [35334, 234],
+        follower_ids: [35334, 234],
+        problem_id: 9873764,
+        has_incidents: false,
+        due_at: null,
+        tags: ["enterprise", "other_tag", "user_workday", "ps_sre_monitoring"],
+        via: {
+          channel: "web"
+        },
+        custom_fields: [
+          {
+            id: 27642,
+            value: "745"
+          },
+          {
+            id: 27648,
+            value: "yes"
+          }
+        ],
+        satisfaction_rating: {
+          id: 1234,
+          score: "good",
+          comment: "Great support!"
+        },
+        sharing_agreement_ids: [84432]
+      },
+      {
+        id: 35438,
+        url: "https://company.zendesk.com/api/v2/tickets/35436.json",
+        external_id: "ahg35h3jh",
+        created_at: "2009-07-20T22:55:29Z",
+        updated_at: "2011-05-05T10:38:52Z",
+        type: "incident",
+        subject: "Help, my printer is on fire!",
+        raw_subject: "{{dc.printer_on_fire}}",
+        description: "The fire is very colorful.",
+        priority: "high",
+        status: "closed",
+        recipient: "support@company.com",
+        requester_id: 20978392,
+        submitter_id: 76872,
+        assignee_id: 235323,
+        organization_id: 509974,
+        group_id: 98738,
+        collaborator_ids: [35334, 234],
+        follower_ids: [35334, 234],
+        problem_id: 9873764,
+        has_incidents: false,
+        due_at: null,
+        tags: ["enterprise", "other_tag", "user_workday", "ps_sre_monitoring"],
+        via: {
+          channel: "web"
+        },
+        custom_fields: [
+          {
+            id: 27642,
+            value: "745"
+          },
+          {
+            id: 27648,
+            value: "yes"
+          }
+        ],
+        satisfaction_rating: {
+          id: 1234,
+          score: "good",
+          comment: "Great support!"
+        },
+        sharing_agreement_ids: [84432]
+      }
+    ]
+  });
+});
+
+module.exports = server;
